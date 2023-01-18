599e1f63f1af8d526548574dca31030251c6a6dd
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-01-18 12:05:11 +0100

    push

--- src/components/SingleResult.jsx
+++ src/components/SingleResult.jsx
@@ -1,11 +1,15 @@
-import './componentsStyles/SingleResult/singleResult.scss';
+import './componentsStyles/SingleResult/singleResult.css';
 
-const SingleResult = () => {
+const SingleResult = ({titolo, descrizione}) => {
     return (
         <div>
-            
+            <div className='custom-result'>
+                <h3>{titolo}</h3>
+                <p>{descrizione}</p>
+            </div>
         </div>
     )
 }
 
 export default SingleResult;
+
--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,9 +1,26 @@
 import '../components/componentsStyles/resultStyle/result.css';
+import SingleResult from './SingleResult';
+
+const fakeData = [
+    {titolo: 'Libro 1', descrizione: 'lorem ipsumkoenfienoeigbnerjkgvnrklgnrkjgnbrjkgnrklgfnrklgnrikojgn'},
+    {titolo: 'Libro 2', descrizione: 'lorem ipsumkoenfienoeigbnerjkgvnrklgnrkjgnbrjkgnrklgfnrklgnrikojgn'},
+    {titolo: 'Libro 3', descrizione: 'lorem ipsumkoenfienoeigbnerjkgvnrklgnrkjgnbrjkgnrklgfnrklgnrikojgn'}
+]
 
 const Result = () => {
+    const renderElement = () => {
+        return fakeData.map((book) => {
+            return <SingleResult/>
+        })
+    }
     return (
         <div>
             <h1>Risultati</h1>
+
+            <div className='results-container'>
+                {renderElement}
+            </div>
+
         </div>
     )
 }
